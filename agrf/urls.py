31465ee98692137e635ebae51646dfbe0d9f057c
Martin Paulo <martin.paulo@gmail.com>
2017-06-16 14:24:26 +1000

    Changed the site header and title

--- agrf/urls.py
+++ agrf/urls.py
@@ -5,6 +5,9 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.conf.urls import url, include
 from django.contrib import admin
 
+admin.site.site_header = 'AGRF Data Importer'
+admin.site.site_title = 'AGRF'
+
 urlpatterns = [
     url(r'^uom_admin/', admin.site.urls, name='admin'),
     # Add Django site authentication urls (for login, logout, etc...)
